b8c3957f0b15561b51930b55a95e0e8b5faf394f
kelvin teixeira <kelvin.teixeira.dev@gmail.com>
2024-04-19 15:23:37 -0300

    feat: Add firstSection with carousel

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -1,3 +1,67 @@
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  "../images/carousel1.jpeg",
+  "../images/carousel6.jpg",
+  "../images/carousel4.jpg",
+  "../images/carousel3.jpg",
+  "../images/carousel2.jpg",
+  "../images/carousel5.jpg",
+];
+
 export const FirstSection = () => {
-  return <h1>Hellow first section</h1>;
+  const [index, setIndex] = useState(0);
+
+  const isMobile = useMediaQuery("(max-width:500px)");
+  const handleChangeIndex = (index: number) => {
+    setIndex(index);
+  };
+  return (
+    <Grid sx={{ width: "100%", height: "70vh" }}>
+      <AutoPlaySwipeableViews index={index} onChangeIndex={handleChangeIndex}>
+        {images.map((image: string, i: number) => (
+          <Grid
+            sx={{
+              width: "100%",
+              height: isMobile ? "auto" : "75vh",
+              overflow: "hidden",
+            }}
+            key={i}
+          >
+            {Math.abs(index - i) <= 2 ? (
+              <img
+                style={{
+                  width: "100%",
+                  height: isMobile ? "50%" : "100%",
+                  filter: "blur(4px)",
+                  objectFit: "cover",
+                }}
+                src={image}
+                alt={`Slide ${i}`}
+              />
+            ) : null}
+          </Grid>
+        ))}
+      </AutoPlaySwipeableViews>
+      <Box
+        style={{
+          position: isMobile ? "relative" : "absolute",
+          top: isMobile ? 0 : 100,
+          margin: isMobile ? 10 : 50,
+        }}
+      >
+        <Typography fontSize={isMobile ? 25 : 60}>Clínica Bellevita</Typography>
+        <Typography maxWidth={800} fontSize={isMobile ? 12 : 20}>
+          Tratamentos exclusivos e eficazes. Nosso objetivo é ajudar você a
+          alcançar o bem-estar usando as técnicas mais recentes combinadas com
+          produtos naturais personalizados especialmente para você.
+        </Typography>
+      </Box>
+    </Grid>
+  );
 };
